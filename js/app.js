2a7059d4bbe92911561c42a210c3049d655308d3
austin ezell <austin.r.ezell@gmail.com>
2015-12-27 19:30:07 -0800

    adds scrolling functions to page directory navbar

--- js/app.js
+++ js/app.js
@@ -5,5 +5,14 @@ var $document = $(document)
 
 $document.ready(function(){
   $('#message').val('')
+  const $siteNavLinks = $("#siteNav a")
+
+  $siteNavLinks.on("click", function(event){
+    event.preventDefault();
+    const location  = $(this).attr("href")
+    $('body, html').stop().animate({
+      scrollTop: $(location).offset().top - 59
+    }, 1500, 'easeOutCirc');
+  })
 
 })
